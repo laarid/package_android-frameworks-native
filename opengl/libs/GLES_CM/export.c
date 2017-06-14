2e153660822caca521c90e23c8da54e5e45c2f21
You-Sheng Yang <vicamo@gmail.com>
2017-06-15 00:21:11 +0800

    opengl: add wrappers for surfaceflinger
    MESA deliberately hides some framebuffer related symbols that are
    required by surfaceflinger. See
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=754397

--- opengl/libs/GLES_CM/export.c
+++ opengl/libs/GLES_CM/export.c
@@ -16,4 +16,64 @@ glEGLImageTargetTexture2DOES(GLenum target, GLeglImageOES image)
         pfn(target, image);
 }
 
+void GL_APIENTRY
+glBindFramebufferOES (GLenum target, GLuint framebuffer)
+{
+    static PFNGLBINDRENDERBUFFEROESPROC pfn = NULL;
+
+    if (!pfn)
+        pfn = (PFNGLBINDRENDERBUFFEROESPROC) eglGetProcAddress("glBindFramebufferOES");
+
+    // MESA has glBindFramebufferOES but not exported.
+    pfn(target, framebuffer);
+}
+
+GLenum GL_APIENTRY
+glCheckFramebufferStatusOES (GLenum target)
+{
+    static PFNGLCHECKFRAMEBUFFERSTATUSOESPROC pfn = NULL;
+
+    if (!pfn)
+        (PFNGLCHECKFRAMEBUFFERSTATUSOESPROC) eglGetProcAddress("glCheckFramebufferStatusOES");
+
+    // MESA has glCheckFramebufferStatusOES but not exported.
+    return pfn(target);
+}
+
+void GL_APIENTRY
+glDeleteFramebuffersOES (GLsizei n, const GLuint* framebuffers)
+{
+    static PFNGLDELETERENDERBUFFERSOESPROC pfn = NULL;
+
+    if (!pfn)
+        pfn = (PFNGLDELETERENDERBUFFERSOESPROC) eglGetProcAddress("glDeleteFramebuffersOES");
+
+    // MESA has glDeleteFramebuffersOES but not exported.
+    pfn(n, framebuffers);
+}
+
+void GL_APIENTRY
+glFramebufferTexture2DOES (GLenum target, GLenum attachment, GLenum textarget, GLuint texture, GLint level)
+{
+    static PFNGLFRAMEBUFFERTEXTURE2DOESPROC pfn = NULL;
+
+    if (!pfn)
+        pfn = (PFNGLFRAMEBUFFERTEXTURE2DOESPROC) eglGetProcAddress("glFramebufferTexture2DOES");
+
+    // MESA has glFramebufferTexture2DOES but not exported.
+    pfn(target, attachment, textarget, texture, level);
+}
+
+void GL_APIENTRY
+glGenFramebuffersOES (GLsizei n, GLuint* framebuffers)
+{
+    static PFNGLGENFRAMEBUFFERSOESPROC pfn = NULL;
+
+    if (!pfn)
+        pfn = (PFNGLGENFRAMEBUFFERSOESPROC) eglGetProcAddress("glGenFramebuffersOES");
+
+    // MESA has glGenFramebuffersOES but not exported.
+    pfn(n, framebuffers);
+}
+
 #endif // !__ANDROID__
